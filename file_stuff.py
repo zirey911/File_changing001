db612568030571a17d578dce8b6669861c5e4fc9
zirey911 <ronshergill2010@gmail.com>
2024-10-25 08:35:05 +0400

    file_stuff.py

--- file_stuff.py
+++ file_stuff.py
@@ -1,6 +1,5 @@
-file = open("matter.txt", "rt")
-data = file.read()
-for letter in data:
-    print(letter, end="#")
-file.close()
-/Users/ronshergill/VSCODE_PROJECTS/file_stuff.py
+file = open("matter.txt", "rt") # change matter.txt to file name or whatever idc. Sigma
+data = file.read() # if you want to read full lines and not like one by one, use file.readline(), it stores all the readded thingies into varible called data
+for letter in data:#for loop for cool people
+    print(letter) #prints all leters
+file.close() # just the close the file yk
