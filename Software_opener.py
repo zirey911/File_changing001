212e5f93df89380902d27a0a29260133273d2554
zirey911 <ronshergill2010@gmail.com>
2024-10-25 08:38:20 +0400

    Add files via upload

---
+++ Software_opener.py
@@ -0,0 +1,5 @@
+import subprocess # lib name
+
+
+# Replace 'notepad' with the name of the application you want to open
+subprocess.run(["open", "-a", "Notes"]) # change safari to app name, MAKE IT SO ALL THE SPACES CAPITALS EVERUYTHING IS THE SAME DO NOT FORGET THIS
